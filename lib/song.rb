81f5023d988ca4465781793a61955f62246ed48d
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2021-01-17 19:55:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,14 +17,6 @@ class Song
   def self.all
     @@songs
   end
-  # 
-  # def self.reset_all
-  #   self.all.clear
-  # end
-  #
-  # def self.count
-  #   self.all.count
-  # end
 
   def artist=(artist)
     @artist = artist
